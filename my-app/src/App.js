3bc73d99a7fc236f4c9f53a7f027ce4a85be480b
TheRitikDubey <www.ritikdubey@gmail.com>
2022-04-06 17:32:06 +0530

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
-import Banner from './Components/Banner/Banner';
-import Categories_Banner from './Components/Categories_Banner/Categories_Banner';
-import Navbar from './Components/Navbar/Navbar';
+// import Banner from './Components/Banner/Banner';
+// import Categories_Banner from './Components/Categories_Banner/Categories_Banner';
+// import Navbar from './Components/Navbar/Navbar';
+import Index from './Components/Pages/Index.js'
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/>
       <Banner/>
       <Categories_Banner catagroy={"Men"}/>
       <Categories_Banner catagroy={"Women"}/>
       <Categories_Banner catagroy={"Kid"}/>
+       */}
+       <Index/>
     </div>
   );
 }
